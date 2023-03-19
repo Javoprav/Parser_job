915a3ba82eab999d690a438c89ddd97e2e00add4
Javoprav <Javoprav@gmail.com>
2023-03-19 14:39:07 +0300

    add save json for hh.ru and superjob.ru

--- classes.py
+++ classes.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from pprint import pprint
 import requests
 
 
@@ -18,12 +19,30 @@ class HH(Engine):
         self.name = None
 
     def get_request(self, name):
+        """Получает json с вакансиями по api"""
         self.name = name
         url = 'https://api.hh.ru/vacancies'
-        params = {'text': self.name, 'area': '1'}
+        params = {'text': self.name, 'area': 113, 'page': 0}
         response = requests.get(url, params=params)
-        vacancies = response.json()
-        return vacancies
+        list_hh = []
+        if response.ok:
+            data = response.json()
+            pages = data['pages']
+            for page in range(pages):
+                params['page'] = page
+                if response.ok:
+                    data = response.json()
+                    vacancies = data['items']
+                    # return vacancies
+                    for vacancy in vacancies:
+                        list_hh.append(vacancy)
+            return list_hh
+        else:
+            pprint('Error:', response.status_code)
+        # vacancies = response.json()
+        # for vacancy in vacancies['items']:
+        #     pprint(vacancy)
+        # return vacancies
 
 
 class Superjob(Engine):
@@ -31,9 +50,10 @@ class Superjob(Engine):
         self.name = None
 
     def get_request(self, name):
+        """Получает json с вакансиями по api"""
         self.name = name
         url2 = 'https://api.superjob.ru/2.0/vacancies/'
-        params2 = {'keyword': self.name, 'geo[c][0]': '1', 'geo[c][1]': '4'}
+        params2 = {'town': '4', 'keyword': self.name, "count": 10000, 'geo[c][0]': '1', 'geo[c][1]': '4'}
         headers2 = {'X-Api-App-Id': 'v3.r.137434972.ee8a700b3805844e09b585e96390378700ad3dd3.996bd4d06d883979e271162feb034b2ed0b00da2'}
         response2 = requests.get(url2, headers=headers2, params=params2)
         vacancies2 = response2.json()
--- main.py
+++ main.py
@@ -5,6 +5,13 @@ from classes import *
 hh = HH()
 sj = Superjob()
 if __name__ == '__main__':
-    user_input = input('Введите название вакнсии: ')
-    pprint(hh.get_request(user_input))
-    pprint(sj.get_request(user_input))
+    user_input = input('Введите название вакансии: ')
+    hh_json = hh.get_request(user_input)
+    sj_json = sj.get_request(user_input)
+    with open('hh.json', 'w', encoding='utf8') as f:
+        json.dump(hh_json, f,  ensure_ascii=False)
+        f.close()
+    # pprint(sj_json)
+    with open('sj.json', 'w', encoding='utf8') as f:
+        json.dump(sj_json, f, ensure_ascii=False)
+        f.close()
